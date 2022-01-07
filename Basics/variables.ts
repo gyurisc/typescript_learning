9b47d0823391f79f054941a8aa817fff0d31f27a
gyurisc <gyurisc@gmail.com>
2022-01-07 11:11:57 +0100

    more variables

--- Basics/variables.ts
+++ Basics/variables.ts
@@ -7,3 +7,45 @@ let nothing: undefined = undefined;
 
 // built in objects
 let now: Date = new Date();
+
+// Array 
+let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+
+// Classes
+class Car {
+
+}
+
+let car: Car = new Car();
+
+// Object literal 
+let point: { x: number; y: number } = {
+    x: 10,
+    y: 20
+};
+
+// Function 
+const logNumber: (i: number) => void = (i: number) => {
+    console.log(i);
+}
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); // {x: 10, y: 20}
+
+coordinates.x = 30;
+coordinates.y = 20;
+
+let words = ["red", "green", "blue"];
+let foundWord: boolean;
+
+for(let i = 0; i < words.length; i++) {
+    if(words[i] === "green") {
+        foundWord = true;
+    }
+}
