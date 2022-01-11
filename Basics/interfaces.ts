6dac94a413f8b15577d21941bbdfd48e744dd16f
gyurisc <gyurisc@gmail.com>
2022-01-11 10:00:02 +0100

    starting with interfaces

---
+++ Basics/interfaces.ts
@@ -0,0 +1,20 @@
+interface Vehicle {
+    name: string; 
+    year: number; 
+    broken: boolean;
+}
+
+const oldCivic : Vehicle = {
+    name: 'civic',
+    year: 2000,
+    broken: true 
+}; 
+
+
+const printVehicle = (vehicle: Vehicle) => {
+    console.log(`Name: ${vehicle.name}`);
+    console.log(`Year: ${vehicle.year}`);
+    console.log(`Broken?: ${vehicle.broken}`);
+}
+
+printVehicle(oldCivic);
