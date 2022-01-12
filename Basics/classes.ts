79cc03e5dcf35eba655322e9731a5ff0a469ad25
gyurisc <gyurisc@gmail.com>
2022-01-12 09:49:39 +0100

    public methods

--- Basics/classes.ts
+++ Basics/classes.ts
@@ -1,8 +1,23 @@
 class Vehicle {
-    drive(): void {
+    public drive(): void {
         console.log('driving');
+    }
+
+    public honk(): void {
+        console.log('honking');
+    }
+}
+
+class Car extends Vehicle {
+    public drive(): void {
+        console.log('driving car');
     }
 }
 
 const vehicle = new Vehicle();
 vehicle.drive();
+vehicle.honk();
+
+const car = new Car();
+car.drive();
+car.honk();
