8cc887ac38c6763d481789f398a10bc05839ae13
gyurisc <gyurisc@gmail.com>
2022-01-13 12:29:10 +0100

    classes and faker

---
+++ maps/src/index.ts
@@ -0,0 +1,10 @@
+import { User } from './User';
+import { Company } from './company';
+
+const u = new User();
+console.log(u.name);
+console.log(u.location);
+
+const company = new Company();
+console.log(company.name);
+console.log(company.catchPhrase);
