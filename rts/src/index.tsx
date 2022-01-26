901be87e71690c87ce5c58ecdac44c867a1ff52e
gyurisc <gyurisc@gmail.com>
2022-01-26 11:59:30 +0100

    adding ref version of usersearch

--- rts/src/index.tsx
+++ rts/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { GuestList } from './state/GuestList';
-import UserSearch from "./state/UserSearch";
+import UserSearch from "./refs/UserSearch";
 
 const App = () => {
   return (
