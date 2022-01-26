5160070442f6e455ba515f42332cb110b4bc8f7a
gyurisc <gyurisc@gmail.com>
2022-01-26 11:54:31 +0100

    refs

---
+++ rts/src/refs/UserSearch.tsx
@@ -0,0 +1,43 @@
+import { useState, useRef, useEffect } from 'react';
+
+
+const users = [
+    { name: 'Sam', age: 30 },
+    { name: 'Alex', age: 20 },
+    { name: 'Micah', age: 40 },
+    { name: 'Alexa', age: 20 },
+    { name: 'Miariam', age: 50 },
+];
+
+const UserSearch: React.FC = () => {
+    const inputRef = useRef<HTMLInputElement | null>(null);
+    const [name, setName] = useState('');
+    const [result, setResult] = useState<{ name: string; age: number } | undefined>();
+    
+    useEffect(() => {
+        inputRef.current?.focus();
+    }, []);
+
+    const searchUser = () => {
+        const foundUser = users.find((user) => { return user.name.toLowerCase() === name.toLowerCase() });
+
+        if (foundUser) {
+            setResult(foundUser);
+        }
+    }
+
+    return (<div>
+        <h3>User Search</h3>
+        <input ref={inputRef} value={name} onChange={(e) => setName(e.target.value)} />
+        <button onClick={searchUser}>Search</button>
+        <div>
+            {result && result.name}
+        </div>
+        <div>
+            {result && result.age}
+        </div>
+    </div >
+    );
+};
+
+export default UserSearch;
