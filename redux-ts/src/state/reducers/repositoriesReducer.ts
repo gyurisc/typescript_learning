7415541152224c9f0db1033bf970b42f3f13ba71
gyurisc <gyurisc@gmail.com>
2022-02-01 14:18:56 +0100

    adding action types

--- redux-ts/src/state/reducers/repositoriesReducer.ts
+++ redux-ts/src/state/reducers/repositoriesReducer.ts
@@ -4,14 +4,43 @@ interface RepositoriesState {
     data: string[]; 
 }
 
-const reducer = (state : RepositoriesState, action: any) => {
+enum ActionType {
+    SEARCH_REPOSITORIES = 'search_repsitories',
+    SEARCH_REPOSITORIES_SUCCESS = 'search_repsitories_success',
+    SEARCH_REPOSITORIES_ERROR = 'search_repsitories_error'
+}
+
+interface SearchRepositoriesAction {
+    type: ActionType.SEARCH_REPOSITORIES;
+}
+
+interface SearchRepositoriesSuccessAction {
+    type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
+    payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+    type: ActionType.SEARCH_REPOSITORIES_ERROR;
+    payload: string;
+}
+
+type Action = 
+| SearchRepositoriesAction 
+| SearchRepositoriesSuccessAction 
+| SearchRepositoriesErrorAction;
+
+const reducer = (
+    state : RepositoriesState, 
+    action: Action
+    
+    ) : RepositoriesState => {
     switch (action.type) {
-        case 'search_repositories':
-            return { loading: true, erro: null, data: [] };
-        case 'search_repositories_success':                
-            return { loading: false, erro: null, data: action.payload };
-        case 'search_repositories_error':
-            return { loading: false, erro: action.payload, data: [] };
+        case ActionType.SEARCH_REPOSITORIES:
+            return { loading: true, error: null, data: [] };
+        case ActionType.SEARCH_REPOSITORIES_SUCCESS:                
+            return { loading: false, error: null, data: action.payload };
+        case ActionType.SEARCH_REPOSITORIES_ERROR:
+            return { loading: false, error: action.payload, data: [] };
         default: 
             return state; 
     }
