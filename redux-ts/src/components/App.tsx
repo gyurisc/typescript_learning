911a3683c374098d85fcc01908e0a5607e35a2dc
gyurisc <gyurisc@gmail.com>
2022-02-04 11:43:17 +0100

    finishing module

--- redux-ts/src/components/App.tsx
+++ redux-ts/src/components/App.tsx
@@ -1,6 +1,7 @@
 import { Provider } from 'react-redux';
 import { store } from '../state'; 
 import RepositoriesList from './RepositoriesList';
+
 const App = () => {
   return (
     <Provider store={store}>
