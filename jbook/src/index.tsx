65bf7c2a38e0bab3ceff857f9c9818dc685fa395
gyurisc <gyurisc@gmail.com>
2022-02-11 11:20:46 +0100

    init esbuild

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,6 +1,39 @@
-import React from 'react';
+import * as esbuild from 'esbuild-wasm'; 
+import {useState, useEffect } from 'react'; 
 import ReactDOM from 'react-dom';
-import App from './App';
+
+function App() {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const startService = async () => {
+    const service = await esbuild.startService({
+      worker: true, 
+      wasmURL: '/esbuild.wasm'
+    });
+    console.log(service);
+  }; 
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = () => {
+    console.log(input);
+  }; 
+
+  return (
+    <div>
+      <textarea onChange={e => setInput(e.target.value)}>{input}</textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>        
+      </div>
+      <pre>
+        {code}
+      </pre>
+    </div>
+  );
+}
 
 
 ReactDOM.render(
