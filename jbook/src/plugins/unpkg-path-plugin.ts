ade134b27400ad3fc109e320c703e258e6f334f5
gyurisc <gyurisc@gmail.com>
2022-02-25 09:33:07 +0100

    using input instead of hardcoded error

--- jbook/src/plugins/unpkg-path-plugin.ts
+++ jbook/src/plugins/unpkg-path-plugin.ts
@@ -7,7 +7,7 @@ const fileCache = localForage.createInstance({
 });
 
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
@@ -42,10 +42,7 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              import React, { useState } from 'react-select';
-              console.log(React, useState);
-            `,
+            contents: inputCode,
           };
         }
 
