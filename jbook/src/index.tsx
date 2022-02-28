36253d8e6d215b60377a3d77e84075d1f34ea581
gyurisc <gyurisc@gmail.com>
2022-02-28 11:59:09 +0100

    fetch module

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -2,6 +2,7 @@ import * as esbuild from 'esbuild-wasm';
 import {useState, useEffect, useRef } from 'react'; 
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
@@ -13,46 +14,44 @@ const App = () => {
       worker: true,
       wasmURL: '/esbuild.wasm',
     });
-  }; 
-
+  };
   useEffect(() => {
     startService();
   }, []);
 
   const onClick = async () => {
-    if(!ref.current) {
-      return; 
+    if (!ref.current) {
+      return;
     }
 
     const result = await ref.current.build({
-      entryPoints: ['index.js'], 
-      bundle: true, 
-      write: false, 
-      plugins: [unpkgPathPlugin(input)],
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
         'process.env.NODE_ENV': '"production"',
-        global: 'window'
-      }
+        global: 'window',
+      },
     });
 
+    // console.log(result);
+
     setCode(result.outputFiles[0].text);
-  }; 
+  };
 
   return (
     <div>
-      <textarea onChange={e => setInput(e.target.value)}>{input}</textarea>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
       <div>
-        <button onClick={onClick}>Submit</button>        
+        <button onClick={onClick}>Submit</button>
       </div>
-      <pre>
-        {code}
-      </pre>
+      <pre>{code}</pre>
     </div>
   );
-}
+};
 
-
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
