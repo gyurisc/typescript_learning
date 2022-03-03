1ba6845aac64be6c18f734b4963f14edc4dbf975
gyurisc <gyurisc@gmail.com>
2022-03-03 10:45:06 +0100

    adding execution to iframe

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -35,10 +35,14 @@ const App = () => {
       },
     });
 
-    // console.log(result);
-
     setCode(result.outputFiles[0].text);
   };
+
+  const html =`
+    <script>
+      ${code}
+    </script>
+  `;
 
   return (
     <div>
@@ -50,6 +54,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe sandbox="allow-scripts" srcDoc={html}></iframe>
     </div>
   );
 };
