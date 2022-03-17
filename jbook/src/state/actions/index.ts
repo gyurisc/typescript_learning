9765ac9876e6c9362ac96edc74058a533c6de081
gyurisc <gyurisc@gmail.com>
2022-03-17 11:45:32 +0100

    cell types

--- jbook/src/state/actions/index.ts
+++ jbook/src/state/actions/index.ts
@@ -1,4 +1,5 @@
 import { ActionType } from '../action-types';
+import { CellTypes } from '../cell';
 
 interface MoveCellAction {
     type: ActionType.MOVE_CELL;
@@ -19,7 +20,7 @@ interface InsertCellBeforeAction {
     type: ActionType.INSERT_CELL_BEFORE;
     payload: {
         id: string;
-        type: 'code' | 'text';
+        type: CellTypes;
     }
 }
 
--- jbook/src/state/cell.ts
+++ jbook/src/state/cell.ts
@@ -1,5 +1,6 @@
+export type CellTypes = 'code' | 'text';
 export interface Cell {
     id: string;
-    type: 'code' | 'text';
+    type: CellTypes;
     content: string;
 }
