746f379829a5077036516728f774398ede9c58fc
gyurisc <gyurisc@gmail.com>
2022-03-23 09:25:43 +0100

    using state in editors

--- jbook/src/components/cell-list-item.tsx
+++ jbook/src/components/cell-list-item.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import './cell-list-item.css';
+import { Cell } from '../state';
+import CodeCell from './code-cell';
+import TextEditor from './text-editor';
+import ActionBar from './action-bar';
 
-const CellListItem: React.FC = () => {
-    return (
-        <div>
-        </ div>
-    );
+interface CellListItemProps {
+  cell: Cell;
 }
+
+const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
+  let child: JSX.Element;
+  if (cell.type === 'code') {
+    child = (
+      <>
+        <div className="action-bar-wrapper">
+          <ActionBar id={cell.id} />
+        </div>
+        <CodeCell cell={cell} />
+      </>
+    );
+  } else {
+    child = (
+      <>
+        <TextEditor cell={cell} />
+        <ActionBar id={cell.id} />
+      </>
+    );
+  }
+
+  return <div className="cell-list-item">{child}</div>;
+};
 
 export default CellListItem;
