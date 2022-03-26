7ddf42f6f08a160dbdce90877a9e24b48f4e63b9
gyurisc <gyurisc@gmail.com>
2022-03-26 07:59:47 +0100

    add cell styling

--- jbook/src/components/add-cell.tsx
+++ jbook/src/components/add-cell.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import './add-cell.css'; 
 import { useActions } from '../hooks/use-actions';
 
 interface AddCellProps {
@@ -7,8 +7,30 @@ interface AddCellProps {
 }
 
 const AddCell: React.FC<AddCellProps> = ({ forceVisible, nextCellId }) => {
+  const { insertCellBefore } = useActions();
+
   return (
-    <div></div>
+    <div className={`add-cell ${forceVisible && 'force-visible'}`}>
+      <div className="add-buttons">
+        <button 
+            className="button is-rounded is-primary is-small" 
+            onClick={() => insertCellBefore(nextCellId, 'code')}>
+          <span className="icon is-small">
+            <i className="fas fa-plus" />            
+          </span>
+          <span>Code</span>
+        </button>
+        <button 
+          className="button is-rounded is-primary is-small" 
+          onClick={() => insertCellBefore(nextCellId, 'text')}>
+        <span className="icon is-small">
+            <i className="fas fa-plus" />            
+          </span>
+          <span>Text</span>          
+        </button>
+      </div>
+      <div className="divider"></div>
+    </div>
   )
 }
 
