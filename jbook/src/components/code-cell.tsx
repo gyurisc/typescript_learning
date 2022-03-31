755d2878fbefa1b7b97c748de9f3bee9a090529a
gyurisc <gyurisc@gmail.com>
2022-03-31 09:06:37 +0200

    progress bar for bundling

--- jbook/src/components/code-cell.tsx
+++ jbook/src/components/code-cell.tsx
@@ -1,3 +1,4 @@
+import './code-cell.css'
 import { useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
@@ -40,8 +41,12 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
           />
         </Resizable>
         {
-          !bundle || bundle.loading
-            ? <div>Loading...</div>
+          !bundle || bundle.loading ? (
+            <div className="progress-cover">
+              <progress className="progress is-small is-primary" max="100">
+                Loading...
+              </progress>
+            </div>)
             : <Preview code={bundle.code} err={bundle.err} />
         }
       </div>
