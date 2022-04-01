405224d34aff2c36204ed1d8c16247292302afe4
gyurisc <gyurisc@gmail.com>
2022-04-01 12:14:59 +0200

    fixing flicker

--- jbook/src/components/code-cell.tsx
+++ jbook/src/components/code-cell.tsx
@@ -40,15 +40,17 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
+        <div className="progress-wrapper">
         {
-          !bundle || bundle.loading ? (
-            <div className="progress-cover">
-              <progress className="progress is-small is-primary" max="100">
-                Loading...
-              </progress>
-            </div>)
-            : <Preview code={bundle.code} err={bundle.err} />
-        }
+          !bundle || bundle.loading ? (            
+              <div className="progress-cover">
+                <progress className="progress is-small is-primary" max="100">
+                  Loading...
+                </progress>
+              </div>            
+          ) : (<Preview code={bundle.code} err={bundle.err} />
+        )}
+        </div>
       </div>
     </Resizable>
   );
