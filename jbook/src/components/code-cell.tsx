90087e0fef4f29aedf705b67775c41c714adc5ee
gyurisc <gyurisc@gmail.com>
2022-04-05 10:50:14 +0200

    adding cumulative code

--- jbook/src/components/code-cell.tsx
+++ jbook/src/components/code-cell.tsx
@@ -14,26 +14,53 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+  const cumulativeCode = useTypedSelector((state) => {
+    const { data, order } = state.cells;
+    const orderedCells = order.map((id) => data[id]);
+
+    const cumulativeCode = [
+      `
+        const show = (value) => {
+          document.querySelector('#root').innerHTML = value;
+        };
+      `,
+    ];
+    for (let c of orderedCells) {
+      if (c.type === 'code') {
+        cumulativeCode.push(c.content);
+      }
+      if (c.id === cell.id) {
+        break;
+      }
+    }
+    return cumulativeCode;
+  });
 
   useEffect(() => {
-    if(!bundle)
-    {
-      createBundle(cell.id, cell.content);
+    if (!bundle) {
+      createBundle(cell.id, cumulativeCode.join('\n'));
       return;
     }
-    const timer = setTimeout(async () => {      
-      createBundle(cell.id, cell.content);
+
+    const timer = setTimeout(async () => {
+      createBundle(cell.id, cumulativeCode.join('\n'));
     }, 750);
 
     return () => {
       clearTimeout(timer);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cell.content, cell.id, createBundle]);
+  }, [cumulativeCode.join('\n'), cell.id, createBundle]);
 
   return (
     <Resizable direction="vertical">
-      <div style={{ height: 'calc(100% - 10px)', display: 'flex', flexDirection: 'row' }}>
+      <div
+        style={{
+          height: 'calc(100% - 10px)',
+          display: 'flex',
+          flexDirection: 'row',
+        }}
+      >
         <Resizable direction="horizontal">
           <CodeEditor
             initialValue={cell.content}
@@ -41,15 +68,15 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
           />
         </Resizable>
         <div className="progress-wrapper">
-        {
-          !bundle || bundle.loading ? (            
-              <div className="progress-cover">
-                <progress className="progress is-small is-primary" max="100">
-                  Loading...
-                </progress>
-              </div>            
-          ) : (<Preview code={bundle.code} err={bundle.err} />
-        )}
+          {!bundle || bundle.loading ? (
+            <div className="progress-cover">
+              <progress className="progress is-small is-primary" max="100">
+                Loading
+              </progress>
+            </div>
+          ) : (
+            <Preview code={bundle.code} err={bundle.err} />
+          )}
         </div>
       </div>
     </Resizable>
