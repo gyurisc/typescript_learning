47e2191bb521252a2082966d2c0f67b57d59fdc5
gyurisc <gyurisc@gmail.com>
2022-04-06 10:52:56 +0200

    namespace collision fix

--- jbook/src/components/code-cell.tsx
+++ jbook/src/components/code-cell.tsx
@@ -1,4 +1,4 @@
-import './code-cell.css'
+import './code-cell.css';
 import { useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
@@ -18,25 +18,32 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { data, order } = state.cells;
     const orderedCells = order.map((id) => data[id]);
 
-    const cumulativeCode = [
-      `
-        const show = (value) => {
-          const root = document.querySelector('#root');
+    const showFunc = `
+    import _React from 'react';
+    import _ReactDOM from 'react-dom';
+    var show = (value) => {
+      const root = document.querySelector('#root');
 
-          if (typeof value === 'object') {
-            if (value.$$typeof && value.props) {
-              ReactDOM.render(value, root);
-            } else {
-              root.innerHTML = JSON.stringify(value);
-            }
-          } else {
-            root.innerHTML = value;
-          }
-        };
-      `,
-    ];
+      if (typeof value === 'object') {
+        if (value.$$typeof && value.props) {
+          _ReactDOM.render(value, root);
+        } else {
+          root.innerHTML = JSON.stringify(value);
+        }
+      } else {
+        root.innerHTML = value;
+      }
+    };
+  `;
+    const showFuncNoop = 'var show = () => {}';
+    const cumulativeCode = [];
     for (let c of orderedCells) {
       if (c.type === 'code') {
+        if (c.id === cell.id) {
+          cumulativeCode.push(showFunc);
+        } else {
+          cumulativeCode.push(showFuncNoop);
+        }
         cumulativeCode.push(c.content);
       }
       if (c.id === cell.id) {
