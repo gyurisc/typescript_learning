87bd7ea9b24e17ee90572528e0d00758dbc4f93d
gyurisc <gyurisc@gmail.com>
2022-04-12 06:20:01 +0200

    adding commands to cli

--- jbook/packages/local-api/src/index.ts
+++ jbook/packages/local-api/src/index.ts
@@ -1,3 +1,6 @@
-export default () => {
+export const serve = (port: number, filename: string, dir: string) => {
     console.log('Server is listening...');
+    console.log('serving/fetching cells from', filename);
+    console.log('dir: ', dir);
+    console.log('on port', port);
 };
