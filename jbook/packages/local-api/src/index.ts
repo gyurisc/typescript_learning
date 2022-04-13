ce6e70c91befc3ab4313fb165d120c42fabc4c61
gyurisc <gyurisc@gmail.com>
2022-04-13 07:03:29 +0200

    setting up proxy middleware

--- jbook/packages/local-api/src/index.ts
+++ jbook/packages/local-api/src/index.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export const serve = (port: number, filename: string, dir: string) => {
     const app = express();
 
-    app.listen(port, () => {
-        console.log('listening on port', port);
-    });
+    app.use(createProxyMiddleware({
+        target: 'http://localhost:3000',
+        ws: true,
+        logLevel: 'silent'
+    }));
+
+    return new Promise<void>((resolve, reject) => {
+        app.listen(port, resolve).on('error', reject);
+    }); 
 };
