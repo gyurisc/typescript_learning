7fbbd3674b64f02e918917398403c58b30df5514
gyurisc <gyurisc@gmail.com>
2022-04-20 09:38:30 +0200

    stuff

--- jbook/packages/cli/src/commands/serve.ts
+++ jbook/packages/cli/src/commands/serve.ts
@@ -9,7 +9,7 @@ export const serveCommand = new Command()
     .action(async (filename = 'notebook.js', options: { port: string}) => {
         try {
             const dir = path.join(process.cwd(), path.dirname(filename))
-            await serve(parseInt(options.port), path.basename(filename), dir);
+            await serve(parseInt(options.port), path.basename(filename), dir, false);
             console.log(
                 `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.`
             );
