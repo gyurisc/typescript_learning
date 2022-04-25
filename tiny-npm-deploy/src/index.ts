109d43d9c0b14a7f642f76b6554674baad988a10
gyurisc <gyurisc@gmail.com>
2022-04-25 06:29:18 +0200

    adding tiny package

---
+++ tiny-npm-deploy/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+    res.send('hello');
+});
+
+app.listen(3005, () => {
+    console.log('Listening on 3005');
+});
