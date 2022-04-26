86e6bca65b3368ffc98b4aad64913edf831c2804
gyurisc <gyurisc@gmail.com>
2022-04-26 06:52:36 +0200

    preparing for publish

--- jbook/packages/cli/src/commands/serve.ts
+++ jbook/packages/cli/src/commands/serve.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import { Command } from 'commander'; 
-import { serve }  from 'local-api'; 
+import { serve }  from '@krisgy/local-api'; 
 
 const isProduction = process.env.NODE_ENV === 'production';
 
